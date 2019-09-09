8f5fc3e45188fd30ac672b79aecde73441c55306
xl <123>
2019-09-09 09:38:18 +0800

    修改之後的提交

--- Exam_99util/src/main/java/com/xl/util/Demoutil.java
+++ Exam_99util/src/main/java/com/xl/util/Demoutil.java
@@ -13,4 +13,31 @@ public class Demoutil {
 		String replaceAll2 = replaceAll.replaceAll("\\\\r","<br>");
 		return replaceAll2;
 	}
+	//判斷不爲空
+	public static boolean t2(String strr){
+		return strr!=null && !"".equals(strr);
+	}
+	/*(1)	将\n\r替换成一个\n。（2分）
+	(2)	将\n结尾的这行文本用<p></p>标签包起来。（6分）
+	(3)	将单个\r字符使用<br/>标签替换。（2分）*/
+	public static String t3(String strr){
+		String replaceAll = strr.replaceAll("\\\\n\\\\r","\n");
+		String replaceAll2 = replaceAll.replaceAll("\\\\n","<p></p>");
+		String replaceAll3 = replaceAll2.replaceAll("\\\\r","<br>");
+		return replaceAll3;
+	}
+	//判斷手機號
+	public static boolean t4(String iphone){
+		String i = "^1(3|5|7)\\d{9}$";
+		boolean b = iphone.matches(i);
+		return b;
+		
+	}
+	//判斷郵箱
+	public static boolean t5(String email) {
+		String e = "^\\w*@\\w*.(com|cn)$";
+		boolean c = email.matches(e);
+		return c;
+		
+	}
 }
--- Exam_99util/src/test/java/test/DemoTest.java
+++ Exam_99util/src/test/java/test/DemoTest.java
@@ -14,5 +14,7 @@ public class DemoTest {
 	public void test1(){
 		String t1 = Demoutil.t1("你好\r\n我好\r");
 		System.out.println(t1);
+		String t3 = Demoutil.t3("9月5日一大早，又有了新情况\n\r\r上午9点多，传出中美经贸高级别磋商牵头人通话的消息。\n\r\r\n9月5日上午，中共中央政治局委员、国务院副总理、中美全面经济对话中方牵头人刘鹤应约与美国贸易代表莱特希泽、财政部长姆努钦通话。\n\r\r\n");
+		System.out.println(t3);
 	}
 }
